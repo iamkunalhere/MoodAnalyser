8cae71c6a35ef6744d9230299dd968fbc5465ac9
iamkunalhere <iamkunalhere@gmail.com>
2020-03-29 19:31:48 +0530

    Welcome to Mood Analyser

---
+++ src/main/java/MoodAnalyser.java
@@ -0,0 +1,16 @@
+public class MoodAnalyser {
+    String message;
+
+    public static void main(String args[]) {
+        System.out.println("Mood Analyser");
+    }
+    // Method for analyzing the mood
+    public String moodAnalyserMethod(String message) {
+        if (message.contains("happy")) {
+            return "HAPPY";
+        }
+        else {
+            return "SAD";
+        }
+    }
+}
---
+++ src/test/java/MoodAnalyserTest.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+public class MoodAnalyserTest {
+
+   MoodAnalyser moodAnalyserObject = new MoodAnalyser();
+    // Test for testing Happy mood
+   @Test
+    public void givenMessage_whenHappy_shouldReturnHappy() {
+       String mood=moodAnalyserObject.moodAnalyserMethod("I'm in happy mood");
+       Assert.assertEquals("HAPPY",mood);
+   }
+   // Test for testing Sad mood
+   @Test
+    public void givenMessage_whenSad_shouldReturnSad() {
+       String mood=moodAnalyserObject.moodAnalyserMethod("I'm in sad mood");
+       Assert.assertEquals("SAD",mood);
+   }
+}
