cb94ace119838d0682b8d78d93d570c1d0db6b32
iamkunalhere <iamkunalhere@gmail.com>
2020-03-29 19:38:54 +0530

    Analyse and respond mood is happy or sad

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,6 +1,5 @@
 public class MoodAnalyser {
     String message;
-
     public static void main(String args[]) {
         System.out.println("Mood Analyser");
     }
--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -1,7 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
 public class MoodAnalyserTest {
-
    MoodAnalyser moodAnalyserObject = new MoodAnalyser();
     // Test for testing Happy mood
    @Test
